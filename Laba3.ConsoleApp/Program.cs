8f7dae4b31b74dfa1018c35e03809d47baad0618
Diana <Diana.fedotova2002@mail.ru>
2024-03-04 00:05:10 +0500

    Создание рабочего приложения

--- Laba3.ConsoleApp/Program.cs
+++ Laba3.ConsoleApp/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Laba3.ClassLibrary;
+
 Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Введите длину радиуса меньшего основания усеченного конуса");
+double r = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите длину радиуса большего основания усеченного конуса");
+double R = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите длину образующей усеченного конуса");
+double l = Convert.ToDouble(Console.ReadLine());
+
+double result = new ClassSquare().S(r, R, l);
+Console.WriteLine("Площадь боковой поверхности усеченного конуса равна: " + result);
