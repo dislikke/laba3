0b0ce40c67340bd8a6616e8ad1d517c4d6739f9c
Diana <Diana.fedotova2002@mail.ru>
2024-04-24 22:53:21 +0500

    добавление нового проекта Asp.Net Core

---
+++ Laba3.WebApplication/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Laba3.WebApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Laba3.WebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
