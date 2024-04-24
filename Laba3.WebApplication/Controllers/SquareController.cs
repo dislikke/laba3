f8983f3568dd5388b489c808fa9a2d84325c1b3e
Diana <Diana.fedotova2002@mail.ru>
2024-04-25 00:42:08 +0500

    изменение models и controllers

---
+++ Laba3.WebApplication/Controllers/SquareController.cs
@@ -0,0 +1,31 @@
+﻿using Laba3.ClassLibrary;
+using Microsoft.AspNetCore.Mvc;
+using Laba3.WebApplication.Models;
+
+namespace Laba3.WebApplication.Controllers
+{
+    public class SquareController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index(SquareModel vm)
+        {
+            double SmallRadius = vm.smallRadius;
+            double LargeRadius = vm.largeRadius;
+            double Generatrix = vm.generatrix;
+            double LatSquare = new ClassSquare().S(SmallRadius, LargeRadius, Generatrix);
+            vm.latSquare = LatSquare;
+            return View(vm);
+        }
+
+        [HttpPost]
+        public IActionResult Calculator(IFormCollection collection, SquareModel vm)
+        {
+            double SmallRadius = vm.smallRadius;
+            double LargeRadius = vm.largeRadius;
+            double Generatrix = vm.generatrix;
+            double LatSquare = new ClassSquare().S(SmallRadius, LargeRadius, Generatrix);
+            vm.latSquare = LatSquare;
+            return View(vm);
+        }
+    }
+}
---
+++ Laba3.WebApplication/Models/SquareModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Laba3.WebApplication.Models
+{
+    public class SquareModel
+    {
+        public double smallRadius { get; set; }
+        public double largeRadius { get; set; }
+        public double generatrix { get; set; }
+        public double latSquare { get; set; }
+
+    }
+}
